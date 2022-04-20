2a30f96d1264363361bc7a7e1c2c32b18413b063
spti <tobetodee@gmail.com>
2022-04-20 21:51:22 +0300

    support a; parse text content of img entity into alt

--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@
 1. It will ignore any descendants, specified by the selector: `div#address-container>span#address` won't generate the span with it's attributes: it only will generate the div with it's attributes.
 2. It will ignore the text content: `{text content}` (probably, it won't parse it)
 3. It doesn't support `@emmetio/abbreviaton`'s selector grouping with braces (see (their readme)[https://github.com/emmetio/emmet/tree/master/packages/abbreviation]): the `semtext-pairs` syntax doesn't support nested braces - this `(a>(b>c+d)*4){{an/url}}` will break the parsing.
+
+## `img`: the `alt` attribute
+It will treat first-occurring text in the `img` entity as the `alt` attribute: `(img){{image description}}` will produce `<img alt="image description">`. It will ignore any other entities in the entity, e.g. `(img){{(div){{illegal div}} image description}}` will produce `<img alt=" image description">`.
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import nearley from 'nearley'
 import grammar from 'semtext-pairs_proto.2/src/grammar.js'
 import emmet from 'emmet'
 
-const TAGS = ['div', 'section', 'li', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'p', 'img']
+const TAGS = ['div', 'section', 'li', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'p', 'img', 'a']
+
+function firstOccuringText(node) {
+    return node.find(node => 'text' === node.type)?.text
+}
 
 function renderBottomUp(node, document) {
     // node either has a label, in which case it is an entity, or it is a text (see Processed data)
@@ -13,10 +17,18 @@ function renderBottomUp(node, document) {
 
     console.log("renderBottomUp, nodeData", nodeData)
 
+
     if (!TAGS.includes(nodeData.name)) throw new Error(`the tagname '${nodeData.name}' isn't supported`)
 
     const dom = document.createElement(nodeData.name)
     if (nodeData.attributes) nodeData.attributes.forEach(attr => dom.setAttribute(attr.name, attr.value))
+
+    if ('img' === nodeData.name) {
+        const firstText = firstOccuringText(node.node)
+        if (firstText) dom.setAttribute('alt', firstText)
+
+        return dom
+    }
 
     node.node.forEach(node => {
         const child = renderBottomUp(node, document)
