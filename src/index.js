77bea952607d34a76b2827cbd2e2ed2d5c18cadf
spti <tobetodee@gmail.com>
2022-04-22 14:45:38 +0300

    define and implement spanTextNodes option and spanToTextTextNodes method

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ import nearley from 'nearley'
 import grammar from 'semtext-pairs_proto.2/src/grammar.js'
 import emmet from 'emmet'
 
-const TAGS = ['div', 'section', 'li', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'p', 'img', 'a']
+const TAGS = ['div', 'section', 'li', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'strong', 'span', 'p', 'img', 'a']
 
 function firstOccuringText(node) {
     return node.find(node => 'text' === node.type)?.text
 }
 
-function renderBottomUp(node, document) {
+function renderBottomUp(node, document, options) {
     // node either has a label, in which case it is an entity, or it is a text (see Processed data)
-    if (!node.label) return document.createTextNode(node.text)
+    if (!node.label) {
+        if (!options.spanTextNodes) return document.createTextNode(node.text)
+
+        const dom = document.createElement('span')
+        dom.setAttribute('data-semtext-text', true)
+        dom.innerHTML = node.text
+
+        return dom
+    }
 
     if ('c' === node.label.value) return null
     const nodeData = emmet.parseMarkup(node.label.value, emmet.resolveConfig()).children[0]
 
-    console.log("renderBottomUp, nodeData", nodeData)
-
+    // console.log("renderBottomUp, nodeData", nodeData)
 
     if (!TAGS.includes(nodeData.name)) throw new Error(`the tagname '${nodeData.name}' isn't supported`)
 
@@ -31,18 +38,26 @@ function renderBottomUp(node, document) {
     }
 
     node.node.forEach(node => {
-        const child = renderBottomUp(node, document)
+        const child = renderBottomUp(node, document, options)
         if (child) dom.appendChild(child)
     })
 
     return dom
 }
 
-function convert(text, document) {
+function spanToTextTextNodes(dom, document) {
+    dom.querySelectorAll('[data-semtext-text]').forEach(el => {
+        el.replaceWith(document.createTextNode(el.innerHTML))
+    })
+
+    return dom
+}
+
+function convert(text, document, options) {
     const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     parser.feed(text)
 
-    return renderBottomUp(parser.results[0], document)
+    return renderBottomUp(parser.results[0], document, options || {})
 }
 
-export {convert}
+export {convert, spanToTextTextNodes, TAGS}
